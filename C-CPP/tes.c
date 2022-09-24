eef8daf91d54558c6bcd47b6c1328188ca764ccd
nyferin <anthonyfebrian01@gmail.com>
2022-09-24 12:06:30 +0700

    another commit

---
+++ C-CPP/tes.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void main() {
+    int a;
+    scanf("%d", &a);
+
+    printf("variabel a: %d\n", a);
+}
+/*
+
+Buatlah bentuk huruf pertama dari nama anda dan tiga digit terakhir dari NIM anda!
+Contoh output program:
+     *       *******  *******  *******
+    * *      *     *       *        * 
+   *   *     *     *   *****       *  
+  *******    *     *        *     *   
+ *       *   *     *        *    *    
+*         *  *******  ******    *     
+
+Buat aplikasi kalkulator yang memberikan hasil ouput penjumlahan, pengurangan, perkalian, pembagian dari 2 angka yang diinput.
+
+Contoh ouput program:
+
+Masukkan angka 1: 4
+Masukkan angka 2: 10
+
+Hasil penjumlahan: 14
+Hasil pengurangan: -6
+Hasil perkalian: 40
+Hasil pembagian: 2.5
+
+*/
